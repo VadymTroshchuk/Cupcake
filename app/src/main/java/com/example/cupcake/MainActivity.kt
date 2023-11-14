ebb13f88c06ee9b6ea948dcb6716d8d3c2be70e9
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-11-14 10:58:02 +0200

    add starter code

---
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -0,0 +1,9 @@
+
+package com.example.cupcake
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Activity for cupcake order flow.
+ */
+class MainActivity : AppCompatActivity(R.layout.activity_main)
